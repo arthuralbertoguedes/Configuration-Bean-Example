d923075a4022b665a985874e999e1db9ceb61999
Arthur Guedes <agalbertoguedes@gmail.com>
2022-09-21 20:49:27 -0300

    adicionando configurações setadas pelo application properties

--- src/main/java/br/com/agtec/ConfigurationBeanExample2/Controller/VendasController.java
+++ src/main/java/br/com/agtec/ConfigurationBeanExample2/Controller/VendasController.java
@@ -2,16 +2,22 @@ package br.com.agtec.ConfigurationBeanExample2.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class VendasController {
 
-	@Autowired
-	@Qualifier("projectName")
-	private String projectName;
+	//Para pegar o parâmetro projectName pelo Bean
+//	@Autowired
+//	@Qualifier("projectName")
+//	private String projectName;
 	
+	//Para pegar o parâmetro pelo application properties
+
+	@Value("${nameApplication}")
+	private String projectName;
 	
 	@GetMapping("/configs")
 	public String getConfiguration() {
